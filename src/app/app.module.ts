aeda34de3f6e38069c2330fabb602672a74b8c9b
AlexStorm <info@alexstorm.ru>
2022-12-09 07:36:46 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+/*
+ * This RemoteEntryModule is imported here to allow TS to find the Module during
+ * compilation, allowing it to be included in the built bundle. This is required
+ * for the Module Federation Plugin to expose the Module correctly.
+ * */
+import {NgModule} from '@angular/core';
+
+import {RouterModule} from '@angular/router';
+import {BootstrapComponent, GridState} from "@solenopsys/uimatrix-templates";
+import {createNgxs} from "@solenopsys/lib-storage";
+import {ClusterState} from "@solenopsys/lib-clusters";
+import {RowsState} from "@solenopsys/uimatrix-lists";
+import {HStreamService, HStreamsState, StreamsPool, WsPool} from "@solenopsys/lib-hyperstreams";
+import {APP_BASE_HREF} from "@angular/common";
+import {environment} from "../environments/environment";
+import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, ROUTES_DEV} from "./conf";
+import {TABLES} from "./tables.config";
+
+
+@NgModule({
+  declarations: [
+    ...DECLARATION
+  ],
+  imports: [
+    RouterModule.forRoot(ROUTES_DEV),
+    ...IMPORTS_CONF,
+    ...createNgxs(!environment.production, [
+      ClusterState,
+      RowsState,
+      GridState,
+      HStreamsState
+    ], true),
+  ],
+  providers: [...PROVIDERS_CONF,WsPool,HStreamService,StreamsPool,
+    {provide: 'tables', useValue: TABLES},
+    {provide: 'assets_dir', useValue: ''},
+    {provide: 'single_start', useValue: true},
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
+  bootstrap: [BootstrapComponent],
+})
+export class AppModule {}
---
+++ src/app/entry.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, routesAndRedirect} from "./conf";
+import {createNgxs} from "@solenopsys/lib-storage";
+import {environment} from "../environments/environment";
+
+
+@NgModule({
+
+  declarations: [...DECLARATION],
+  imports: [
+    RouterModule.forChild(routesAndRedirect),
+    ...IMPORTS_CONF,
+    ...createNgxs(!environment.production,[])
+  ],
+  providers: [
+    ...PROVIDERS_CONF,
+    {provide: 'single_start', useValue: false},
+    {provide: 'assets_dir', useValue: '/modules/kubernetes'}
+  ],
+
+})
+export class RemoteEntryModule {}
