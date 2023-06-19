41bbd658ff78ed2362725cb4dc491a0047657d62
AlexStorm <info@alexstorm.ru>
2023-06-19 12:48:18 +0530

    refactor modules

--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -1,19 +1,35 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, routesAndRedirect} from "./conf";
 
+import {NgModule} from '@angular/core';
+
+import {RouterModule} from '@angular/router';
+import {BootstrapComponent, GridState} from "@solenopsys/ui-templates";
+import {ClusterState} from "@solenopsys/fl-clusters";
+import {RowsState} from "@solenopsys/ui-lists";
+import {HStreamService, HStreamsState, StreamsPool, WsPool} from "@solenopsys/fl-hyperstreams";
+import {APP_BASE_HREF} from "@angular/common";
+import {DECLARATION, IMPORTS_CONF, PROVIDERS_CONF, ROUTES_DEV} from "./conf";
+import {TABLES} from "./tables.config";
+
+
+export const STATES=  [
+  ClusterState,
+  RowsState,
+  GridState,
+  HStreamsState
+];
 
 @NgModule({
-
-  declarations: [...DECLARATION],
+  declarations: [
+    ...DECLARATION
+  ],
   imports: [
-    RouterModule.forChild(routesAndRedirect),
-    ...IMPORTS_CONF
+    RouterModule.forRoot(ROUTES_DEV),
+    ...IMPORTS_CONF,
   ],
-  providers: [
-    ...PROVIDERS_CONF,
-    {provide: 'assets_dir', useValue: '/modules/mf-kubernetes'}
+  providers: [...PROVIDERS_CONF,WsPool,HStreamService,StreamsPool,
+    {provide: 'tables', useValue: TABLES},
+    {provide: 'assets_dir', useValue: ''},
+    {provide: APP_BASE_HREF, useValue: '/'}
   ],
-
 })
 export class RemoteEntryModule {}
